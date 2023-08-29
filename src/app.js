77a1fcd7da0627eb33d353a86021bce4ed66aad2
Kun Woo's Mac Mini <kunwookim@Kunui-Macmini.local>
2023-08-29 12:51:56 +0900

    testing

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.json("Hello TKTrade! My Name is Kun Woo Kim")
+    res.json("Hello TKTrade! My Name is Kun Woo Kim! Oh My God")
 });
 
 module.exports = app
